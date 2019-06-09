cbe22ee0ee842e3d2e1e3b3cd292d578cbb3157b
Y Paul Sussman <ypaulsussman@gmail.com>
2019-06-09 13:54:45 -0500

    add seeds; expand search results

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,97 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [{  
+    first_name: "Steve",
+    last_name: "Stevenson",
+    email: "user1@foo.com",
+    password: "password",
+    bio: "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.",
+    can_help: "Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.",
+    need_help: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.",
+    interested_in: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line."
+  }, {  
+    first_name: "William",
+    last_name: "Williamson",
+    email: "user2@foo.com",
+    password: "password",
+    bio: "Polo shirts with popped collars did I do that julia roberts netscape navigator. Wesley snipes flannel shirts pontiac trans sport dennis rodman wayne gretzky korn. Extreme sports i'm king of the world deep blue chandler bing glow in the dark stickers, mia hamm bye bye bye whitney houston digital pets da bomb. Discovery zone airwalk gordon bombay biker shorts personalized mixtapes, babyface air jordans geo metro be kind rewind super nintendo titanic. Ken griffey jr millenials kool-aid man street fighter ii snapback hats nirvana.",
+    can_help: "Gotta have my pops julia roberts the matrix chuck taylor all-stars beanie babies. Street fighter ii tom hanks trippin’ atlanta summer olympics hotmail, ross and rachel turquoise taco bell chihuahua denzel washington bare midriffs. Roseanne barr savage garden neon colors mtv, necklaces with your name on a grain of rice george clooney marky mark teenage mutant ninja turtles smells like teen spirit. Airwalk overalls with one strap undone the more you know michael jordan bomber jacket slap bracelet. Armageddon the phantom menace bye bye bye new jack city this is your brain on drugs pulp fiction.",
+    need_help: "Zebra stripes i'm king of the world super mario world boy meets world wearing your cap backwards, ford taurus teenage mutant ninja turtles yo quiero taco bell discovery zone fila. Aerosmith trl your mom hoodies rayon shirts. Keanu reeves dream team encarta fly, george michael wonderwall tupac shakur renting movies at a store chat rooms. Tamagotchi flannel shirts berry lips george clooney charlotte hornets.",
+    interested_in: "Dawson’s creek gangsta’s paradise gotta have my pops sugar ray acid wash super nintendo. Turquoise nirvana smarter child rayon shirts, flip flops deep blue velcro sneakers spice girls. Crew cut seattle supersonics royal stewart tartan kicks boyz II men your mom, visor wayne gretzky cassette tape blockbuster video oasis. Life is like a box of chocolates I will always love you hip hop arsenio hall minesweeper accent braids."
+  }, {  
+    first_name: "John",
+    last_name: "Johnson",
+    email: "user3@foo.com",
+    password: "password",
+    bio: "Will smith airwalk scrunchies berry lips, skidz and zubas mood rings y2k leotards. Toni braxton I don’t want no scrubs rollerblades vanilla coke hip hop, tamagotchi pixie cut dsl bare midriffs vertically striped shirts pagers. Overalls with one strap undone toy story wearing your cap backwards cory matthews christina aguilera maze screensavers, business casual mariah carey home alone your mom. Rocko’s modern life rush hour schindler’s list ford bronco oregon trail.",
+    can_help: "La gear renting movies at a store hootie and the blowfish mazda mpv. Digital pets chronic did I do that royal stewart tartan. New jack city maze screensavers wesley snipes yo quiero taco bell wearing your cap backwards, atlanta summer olympics bike caps highlights chia pet. Hoop earrings mia hamm I will always love you no fear t-shirts nike, move it football head glow in the dark stickers rayon shirts nickelodeon kazaa atlanta braves.",
+    need_help: "This is your brain on drugs can’t touch this nike jurassic park christina aguilera. Cassette tape light up sneakers george michael turtlenecks bop it, bowl cut cargo pants pixie cut joey tribbiani rachel green kazaa. Personal computer the phantom menace union jack tamagotchi. Buddy list visor overalls with one strap undone julia roberts, carlton dance scrunched socks got milk how do you like them apples instant messaging. Wearing your cap backwards pontiac trans sport nerf guns rayon shirts floppy disk razor scooter.",
+    interested_in: "Britpop nickelodeon wonderwall pogs, neon colors converse super nintendo atlanta braves cargo pants babyface. Sitcoms be kind rewind home skillet once you pop you can’t stop savage garden spiked hair, end of the road yin yang chia pet tupac shakur flat tops internet. Life is like a box of chocolates ghost pottery scene biker shorts fluorescent taco bell chihuahua skidz and zubas. Nike cable modem da bomb denim jackets hook leggings."
+  }, {  
+    first_name: "Peter",
+    last_name: "Peterson",
+    email: "user4@foo.com",
+    password: "password",
+    bio: "Hey, you know how I'm, like, always trying to save the planet? Here's my chance. They're using our own satellites against us. And the clock is ticking. So you two dig up, dig up dinosaurs? Eventually, you do plan to have dinosaurs on your dinosaur tour, right?",
+    can_help: "Hey, you know how I'm, like, always trying to save the planet? Here's my chance. Did he just throw my cat out of the window? We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore!",
+    need_help: "Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.",
+    interested_in: "You really think you can fly that thing? I gave it a cold? I gave it a virus. A computer virus. Hey, you know how I'm, like, always trying to save the planet? Here's my chance. Checkmate... You really think you can fly that thing? Must go faster. Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists."
+  }, {  
+    first_name: "Paul",
+    last_name: "Paulson",
+    email: "user5@foo.com",
+    password: "password",
+    bio: "Remind me to thank John for a lovely weekend. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Hey, you know how I'm, like, always trying to save the planet? Here's my chance. They're using our own satellites against us. And the clock is ticking.",
+    can_help: "Just my luck, no ice. You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? I gave it a cold? I gave it a virus. A computer virus. You really think you can fly that thing? What do they got in there? King Kong?",
+    need_help: "Must go faster... go, go, go, go, go! God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs. This thing comes fully loaded. AM/FM radio, reclining bucket seats, and... power windows. Just my luck, no ice.",
+    interested_in: "Must go faster. What do they got in there? King Kong? Life finds a way. Is this my espresso machine? Wh-what is-h-how did you get my espresso machine? Did he just throw my cat out of the window? Yeah, but John, if The Pirates of the Caribbean breaks down, the pirates don’t eat the tourists."
+  }, {  
+    first_name: "Eric",
+    last_name: "Ericson",
+    email: "user6@foo.com",
+    password: "password",
+    bio: "Sheet crack Jennys tea cup main sheet tackle crimp stern log fathom Chain Shot execution dock. Yawl smartly Plate Fleet plunder sheet sutler Sink me scourge of the seven seas hang the jib Yellow Jack. Port heave down nipperkin Davy Jones' Locker red ensign gunwalls boatswain reef bilged on her anchor walk the plank.",
+    can_help: "Carouser execution dock line scourge of the seven seas bilge water bring a spring upon her cable Sail ho coffer draught lanyard. Draft chase lad fire ship cackle fruit parrel heave down gabion blow the man down execution dock. Hogshead handsomely tack flogging Jolly Roger six pounders hempen halter Privateer killick quarter. ",
+    need_help: "Hogshead Corsair trysail come about quarterdeck Jolly Roger spirits cog bounty case shot. Careen lugger Jack Ketch sutler flogging grog blossom hail-shot furl bucko Blimey. Clap of thunder loaded to the gunwalls hands deadlights sloop hempen halter warp red ensign salmagundi jury mast. ",
+    interested_in: "Lookout Barbary Coast topsail landlubber or just lubber scourge of the seven seas boatswain come about port gally smartly. Yo-ho-ho Admiral of the Black pink tender poop deck wherry splice the main brace log ho cackle fruit. Plate Fleet gabion flogging gally blow the man down Letter of Marque fluke avast deadlights schooner. "
+  }, {  
+    first_name: "Rasmus",
+    last_name: "Rasmussen",
+    email: "user7@foo.com",
+    password: "password",
+    bio: "Jolly boat rum scallywag tackle Nelsons folly topgallant skysail scuppers code of conduct blow the man down. Long boat barque weigh anchor bring a spring upon her cable list gangplank lugsail topgallant hang the jib bilge rat. Cackle fruit Plate Fleet crack Jennys tea cup lee careen starboard yo-ho-ho chase measured fer yer chains line. ",
+    can_help: "League belay cog swing the lead gally six pounders tackle scallywag overhaul Chain Shot. Sail ho log spyglass deadlights ho main sheet Buccaneer smartly spike chase. Long clothes chase swing the lead spike weigh anchor lookout shrouds furl bring a spring upon her cable come about. ",
+    need_help: "Belay brigantine reef cable pressgang pirate salmagundi Sea Legs Gold Road bounty. List transom coffer Admiral of the Black Spanish Main overhaul lass cackle fruit provost capstan. Furl brig ye reef Plate Fleet cog jib chantey provost scurvy. ",
+    interested_in: "Boatswain spanker Admiral of the Black bowsprit loaded to the gunwalls lugger Cat o'nine tails sheet mizzenmast yo-ho-ho. Shiver me timbers jolly boat fore hardtack salmagundi driver rope's end blow the man down swab killick. Aye case shot dance the hempen jig bounty Sea Legs reef lugger long clothes crimp belay. "
+  }, {  
+    first_name: "Jens",
+    last_name: "Jensen",
+    email: "user8@foo.com",
+    password: "password",
+    bio: "Old boys club the last person we talked to said this would be ready. Screw the pooch digital literacy and player-coach yet red flag what the and no scraps hit the floor core competencies. The horse is out of the barn fire up your browser. What the open door policy. Game-plan. Push back quick win. Going forward criticality talk to the slides. Peel the onion feature creep Q1, for pipeline, nor cloud strategy. Close the loop i'll book a meeting so we can solution this before the sprint is over but product management breakout fastworks or gain alignment or big boy pants so hard stop nail it down.",
+    can_help: "We want to empower the team with the right tools and guidance to uplevel our craft and build better my supervisor didn't like the latest revision you gave me can you switch back to the first revision?. Shoot me an email take five, punch the tree, and come back in here with a clear head.",
+    need_help: "Run it up the flag pole we're ahead of the curve on that one yet draft policy ppml proposal product management breakout fastworks. If you want to motivate these clowns, try less carrot and more stick circle back that ipo will be a game-changer high turnaround rate nor organic growth. Commitment to the cause feature creep, or screw the pooch.",
+    interested_in: "Productize if you want to motivate these clowns, try less carrot and more stick, nor can we parallel path, who's responsible for the ask for this request?. Canatics exploratory investigation data masking not a hill to die on but run it up the flag pole critical mass, so we just need to put these last issues to bed, nor helicopter view deep dive. Can we parallel path on your plate going forward, back-end of third quarter put in in a deck for our standup today downselect."
+  }, {  
+    first_name: "Hans",
+    last_name: "Hansen",
+    email: "user9@foo.com",
+    password: "password",
+    bio: "Granularity enough to wash your face pivot. Pull in ten extra bodies to help roll the tortoise even dead cats bounce, viral engagement, nor crank this out but back-end of third quarter and we need to harvest synergy effects. Exposing new ways to evolve our design language prairie dogging viral engagement. Proceduralize it's about managing expectations minimize backwards overflow obviously strategic fit.",
+    can_help: "Bottleneck mice powerpoint Bunny. Onward and upward, productize the deliverables and focus on the bottom line driving the initiative forward nor window of opportunity for vertical integration, but this is a no-brainer we need to touch base off-line before we fire the new ux experience. Level the playing field hard stop run it up the flag pole yet get six alpha pups in here for a focus group, so wiggle room.",
+    need_help: "When does this sunset? those options are already baked in with this model value prop can you send me an invite?, for high performance keywords so ultimate measure of success yet let's not solutionize this right now parking lot it. Closer to the metal. Reach out high turnaround rate or obviously in an ideal world so baseline for turd polishing. Please use 'solutionise instead of solution ideas! :) circle back helicopter view, and dog and pony show. Touch base rock Star/Ninja, or sea change out of the loop. Ramp up crisp ppt for low hanging fruit.",
+    interested_in: "Back-end of third quarter driving the initiative forward and spinning our wheels or we are running out of runway. Nobody's fault it could have been managed better pixel pushing, but please use 'solutionise' instead of solution ideas! :) so what's our go to market strategy? exposing new ways to evolve our design language. Old boys club onward and upward, productize the deliverables and focus on the bottom line accountable talk, for one-sheet this is not the hill i want to die on. This is meaningless gain traction we need to socialize the comms with the wider stakeholder community nor guerrilla marketing, but vertical integration, and run it up the flag pole. Cross functional teams enable out of the box brainstorming we want to see more charts UI organic growth overcome key issues to meet key milestones."
+  }, {  
+    first_name: "Anders",
+    last_name: "Andersen",
+    email: "user10@foo.com",
+    password: "password",
+    bio: "Red flag. Let's unpack that later pig in a python, or high turnaround rate but your work on this project has been really impactful ping me. Punter I just wanted to give you a heads-up even dead cats bounce, yet regroup nor drill down downselect. What about scaling components to a global audience? when does this sunset? and let's unpack that later wheelhouse, nor run it up the flagpole, ping the boss and circle back window-licker where do we stand on the latest client ask.",
+    can_help: "We are running out of runway out of scope, so if you want to motivate these clowns, try less carrot and more stick. Going forward we need more paper hit the ground running. It's not hard guys wheelhouse, or locked and loaded onward and upward, productize the deliverables and focus on the bottom line. Closing these latest prospects is like putting socks on an octopus win-win, nor crank this out, powerpoint Bunny, for globalize, for time to open the kimono digitalize.",
+    need_help: "Cross-pollination organic growth where do we stand on the latest client ask, but translating our vision of having a market leading platfrom out of scope dog and pony show. Back to the drawing-board. Strategic high-level 30,000 ft view peel the onion, and re-inventing the wheel cross functional teams enable out of the box brainstorming. Net net driving the initiative forward or create spaces to explore what’s next or feature creep i also believe it's important for every member to be involved and invested in our company and this is one way to do so nor please advise soonest for highlights . Low engagement low-hanging fruit your work on this project has been really impactful, so paddle on both sides.",
+    interested_in: "Cross functional teams enable out of the box brainstorming bake it in. Blue sky where the metal hits the meat so hard stop programmatically, for low-hanging fruit i’ve been doing some research this morning and we need to better red flag. We need to touch base off-line before we fire the new ux experience. Land the plane clear blue water and pulling teeth, helicopter view, yet dog and pony show."
+  }]
+)
